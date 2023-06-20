ceec2a31be40e273eaf9a9bb8c34007e41a28e65
wesleyoliveira01 <wesleyoliveira.dev@outlook.com>
2023-06-20 17:37:09 -0300

    pegando links com regex

--- index.js
+++ index.js
@@ -1,22 +1,27 @@
 import fs from "fs";
 import chalk from "chalk";
 
+
 const trataErro = (e) => {
   console.log(chalk.red(e));
   throw new Error(chalk.red(e.code, "arquivo não encontrado"));
 };
 
+const pegaTexto = (texto) => {
+  const regex = /\[([^[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)/gm;
+  const capturas = [...texto.matchAll(regex)];
+  const resultado = capturas.map(capturas => ({[capturas[1]]: capturas[2]}))
+  return resultado
+}
+
 const pegaArquivo = async (caminhoArquivo) => {
   const encoding = "utf8";
   try {
     const texto = await fs.promises.readFile(caminhoArquivo, encoding);
-    console.log(chalk.green(texto));
+    console.log(pegaTexto(texto));
   } catch (e) {
     trataErro(e);
   }
 };
 
-
-pegaArquivo("./arquivos/texto.md");
-
-//\[[^[\]]*?\]
+pegaArquivo("./arquivos/texto.md")
