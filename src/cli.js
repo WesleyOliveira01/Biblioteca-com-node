be95c2029829c05db06b9f39e32f1428451e9a1b
wesleyoliveira01 <wesleyoliveira.dev@outlook.com>
2023-06-21 17:25:03 -0300

    pegando regex por direitorio via CLI

--- src/cli.js
+++ src/cli.js
@@ -4,10 +4,22 @@ import pegaArquivo from "./index.js";
 
 const caminho = process.argv;
 
-const processaArquivo = async (caminho) => {
-    const links = await pegaArquivo(caminho[2])
-    console.log(chalk.yellow("lista de links"),links)
+const imprimeLista = (resultado) => {
+    console.log(chalk.yellow("lista de links"), resultado);
 }
 
+const processaArquivo = async (caminho) => {
+  const arquivo = caminho[2];
+  if (fs.lstatSync(arquivo).isFile()) {
+    const links = await pegaArquivo(arquivo);
+    imprimeLista(links);
+  } else if(fs.lstatSync(arquivo).isDirectory()) {
+    const arquivos = await fs.promises.readdir(arquivo)
+    arquivos.forEach(async (links) => {
+       const arquivosDoDiretorio = await pegaArquivo(`./${arquivo}/${links}`)
+       imprimeLista(arquivosDoDiretorio);
+    });
+  }
+};
 
-processaArquivo(caminho)
+processaArquivo(caminho);
